b56be4351615d9c81fe8fc47a4c06c1a0f8c2e94
Mascherpa Alessandro Ernesto <a.mascherpa@computer.org>
2021-05-26 22:45:07 +0200

    development-timings: timing events: batch files for test

---
+++ server/src/test/batch/timer/list.bat
@@ -0,0 +1,1 @@
+curl -v -X GET  "http://localhost:8090/events-collector/events/timings/"
---
+++ server/src/test/batch/timer/store.bat
@@ -0,0 +1,8 @@
+curl -v -X POST  "http://localhost:8090/events-collector/events/timings/"  --data "&id=timer-1&when=2021-05-21T22:05:03.000&elapsed=1.0"
+
+curl -v -X POST  "http://localhost:8090/events-collector/events/timings/"  --data "&id=timer-2&when=2021-05-21T22:15:20.000&elapsed=2.0"
+curl -v -X POST  "http://localhost:8090/events-collector/events/timings/"  --data "&id=timer-2&when=2021-05-21T22:55:22.000&elapsed=4.0"
+
+curl -v -X POST  "http://localhost:8090/events-collector/events/timings/"  --data "&id=timer-3&when=2021-05-22T21:34:45.000&elapsed=1"
+curl -v -X POST  "http://localhost:8090/events-collector/events/timings/"  --data "&id=timer-3&when=2021-05-22T21:52:36.000&elapsed=2"
+curl -v -X POST  "http://localhost:8090/events-collector/events/timings/"  --data "&id=timer-3&when=2021-05-22T21:02:12.000&elapsed=3"
