26d7d40856efecfd605b1f3b5041e1279adbab50
Mascherpa Alessandro Ernesto <a.mascherpa@computer.org>
2022-02-10 17:41:16 +0100

    development: batch to start service updated according configuration

--- startup.bat
+++ startup.bat
@@ -1,4 +1,8 @@
 @echo off
+setlocal
+set EVENTS_COLLECTOR_REPOSITORY_TYPE=postgres
+set EVENTS_COLLECTOR_REPOSITORY_URL=r2dbc:postgresql://postgres:password@localhost:5432/events-collector
+
 pushd  .\server\target
 for %%j in ( events-collector*.jar ) do start java -jar "%%j"
 popd
